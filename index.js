6c7e183b9b7a75898e6babe658c55cab39bb5e5b
itisWasp <mugishaisrael18@gmail.com>
2021-12-20 14:44:46 +0200

    Added Service Page

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
-    
-})
+scrollTo = (element) =>{
+    window.scroll({
+        behavior: "smooth",
+        left: 0,
+        top: element.offsetTop
+    });
+
+    console
+}
+
+document.getElementById("first").addEventListener('click', () => {
+    scrollTo(document.getElementById('services'));
+});
